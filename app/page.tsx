b086712bbb23e7b4b69026f76c66927c9f877bb6
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-07-11 20:42:08 +0530

    added footer and also changed little bugs in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Achivements from "@/components/Achivements";
 import Approach from "@/components/Approach";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import OtherSkills from "@/components/OtherSkills";
@@ -12,7 +13,7 @@ import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center overflow-clip flex-col mx-auto sm:px-10 px-5">
       
       <div className="max-w-7lx w-full">
         <FloatingNav navItems={navItems}/>
@@ -22,6 +23,7 @@ export default function Home() {
         <RecentProjects/>
         <Achivements/>
         <Approach/>
+        <Footer/>
       </div>
     </main>
   );
