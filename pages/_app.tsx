43c0f62e5ab460850155d2497b6409f2834db5ce
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-07-18 12:04:45 +0530

    added style to contact us form

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,13 @@
 import { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 import AppContainer from '../components/AppContainer';
 import theme from '../config/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <AppContainer>
+      <CSSReset/>
         <Component {...pageProps} />
-      </AppContainer>
     </ChakraProvider>
   );
 }
