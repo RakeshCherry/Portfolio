7326e88aa160ed21c77504b317fc942f71829b92
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-07-31 21:02:21 +0530

    .

--- app/LinkedIn/LinkedIn.tsx
+++ app/LinkedIn/LinkedIn.tsx
@@ -13,9 +13,8 @@ const LinkedIn = () => {
   };
 
   return (
-    <div className="h-[100vh] relative w-full overflow-hidden bg-slate-950 flex flex-col items-center justify-center rounded-lg">
-      <div className="absolute inset-0 w-full h-full bg-slate-950 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
-      <BoxesCore />
+    <>
+    <div className='flex flex-col items-center'>
       <TextGenerateEffectLinkedIn
         className='lg:max-w-[70vw] font-Stylish text-center md:tracking-wider custom-sm:text-[1rem] md:text-lg lg:text-2xl mb-4 relative z-20'
         words="Thank you for stopping by my LinkedIn profile! I&apos;m currently taking a brief hiatus from LinkedIn but will be back shortly. In the meantime, I&apos;d love to hear from you via email."
@@ -26,7 +25,8 @@ const LinkedIn = () => {
           Have a <span className='text-yellow-500 text-[1rem] uppercase'>fantastic day</span>, filled with joy and inspiration!
         </p>
       )}
-    </div>
+      </div>
+      </>
   );
 };
 
