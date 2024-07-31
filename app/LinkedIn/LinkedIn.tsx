7de8db28282d241fdf2d21ec7bb7d5334528574b
RAKESH M B <rakeshbhavimani1@gmail.com>
2024-07-31 18:12:25 +0530

    changed font style in LinkedIn component

--- app/LinkedIn/LinkedIn.tsx
+++ app/LinkedIn/LinkedIn.tsx
@@ -10,7 +10,7 @@ const LinkedIn = () => {
                   words='Thank you for stopping by my LinkedIn profile! I&apos;m currently taking a brief hiatus from LinkedIn but will be back shortly. In the meantime, I&apos;d love to hear from you via email.'
             />
     <p className='lg:max-w-[70vw] font-Dancing_Script text-center md:tracking-wider custom-sm:text-[1rem] md:text-lg lg:text-2xl mb-4 dark:text-white'>
-        Have a <span className='text-yellow-300'>fantastic day </span>, filled with joy and inspiration!</p>
+        Have a <span className='text-yellow-500 text-[1rem] uppercase'>fantastic day </span>, filled with joy and inspiration!</p>
     </div>
     </>
   )
